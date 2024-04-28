4be858038b2df33856460dec56c7d959e9bf7ccf
Manuel Scianca <manuelscianca01@gmail.com>
2024-04-28 15:53:49 -0300

    agrego los paths para editar y elimianr productos a urls

--- app/urls.py
+++ app/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path("clientes/eliminar/", view=views.clients_delete, name="clients_delete"),
     path("productos/", view=views.products_repository, name="products_repo"),
     path("productos/nuevo/", view=views.products_form, name="products_form"),
+    path("productos/editar/<int:id>/", view=views.products_form, name="products_edit"),
+    path("productos/eliminar/", view=views.products_delete, name="products_delete"),
 ]
