3fc5b13f308aa8bec79f12fb396fb842af13b512
Pablo Chesini <pchesini1@gmail.com>
2024-04-29 16:02:35 -0300

    [abm/medicine]:Se creó el template de medicine junto con el formulario para la creacion del objeto y su patron repository para la base de datos

--- app/urls.py
+++ app/urls.py
@@ -7,8 +7,14 @@ urlpatterns = [
     path("clientes/nuevo/", view=views.clients_form, name="clients_form"),
     path("clientes/editar/<int:id>/", view=views.clients_form, name="clients_edit"),
     path("clientes/eliminar/", view=views.clients_delete, name="clients_delete"),
+    
     path("veterinarios/", view=views.vets_repository, name="vets_repo"),
     path("veterinarios/nuevo/", view=views.vets_form, name="vets_form"),
     path("veterinarios/editar/<int:id>/", view=views.vets_form, name="vets_edit"),
      path("veterinarios/eliminar/", view=views.vets_delete, name="vets_delete"),
+     
+    path("medicina/", view=views.vets_repository, name="medi_repo"),
+    path("medicina/nuevo/", view=views.vets_form, name="medi_form"),
+    path("medicina/editar/<int:id>/", view=views.vets_form, name="medi_edit"),
+    path("medicina/eliminar/", view=views.vets_delete, name="medi_delete"),  
 ]
