22e9dfcd41f2dce77e22ff79b5969d09b2cfd977
Manuel Scianca <manuelscianca01@gmail.com>
2024-05-26 12:55:42 -0300

    realizo el test de integración

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -5,11 +5,12 @@ from playwright.sync_api import sync_playwright, expect, Browser
 
 from django.urls import reverse
 
-from app.models import Client
+from app.models import Product
 
 os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
 playwright = sync_playwright().start()
 headless = os.environ.get("HEADLESS", 1) == 1
+#headless = os.environ.get("HEADLESS", "0") == 1
 slow_mo = os.environ.get("SLOW_MO", 0)
 
 
@@ -36,124 +37,103 @@ class PlaywrightTestCase(StaticLiveServerTestCase):
 
 
 class HomeTestCase(PlaywrightTestCase):
-    def test_should_have_navbar_with_links(self):
-        self.page.goto(self.live_server_url)
-
-        navbar_home_link = self.page.get_by_test_id("navbar-Home")
-
-        expect(navbar_home_link).to_be_visible()
-        expect(navbar_home_link).to_have_text("Home")
-        expect(navbar_home_link).to_have_attribute("href", reverse("home"))
-
-        navbar_clients_link = self.page.get_by_test_id("navbar-Clientes")
-
-        expect(navbar_clients_link).to_be_visible()
-        expect(navbar_clients_link).to_have_text("Clientes")
-        expect(navbar_clients_link).to_have_attribute("href", reverse("clients_repo"))
-
+    
     def test_should_have_home_cards_with_links(self):
         self.page.goto(self.live_server_url)
 
-        home_clients_link = self.page.get_by_test_id("home-Clientes")
+        home_products_link = self.page.get_by_test_id("home-Productos")
 
-        expect(home_clients_link).to_be_visible()
-        expect(home_clients_link).to_have_text("Clientes")
-        expect(home_clients_link).to_have_attribute("href", reverse("clients_repo"))
+        expect(home_products_link).to_be_visible()
+        expect(home_products_link).to_have_text("Productos")
+        expect(home_products_link).to_have_attribute("href", reverse("products_repo"))
 
 
-class ClientsRepoTestCase(PlaywrightTestCase):
+class ProductsRepoTestCase(PlaywrightTestCase):
     def test_should_show_message_if_table_is_empty(self):
-        self.page.goto(f"{self.live_server_url}{reverse('clients_repo')}")
+        self.page.goto(f"{self.live_server_url}{reverse('products_repo')}")
 
-        expect(self.page.get_by_text("No existen clientes")).to_be_visible()
+        expect(self.page.get_by_text("No existen productos")).to_be_visible()
 
-    def test_should_show_clients_data(self):
-        Client.objects.create(
-            name="Juan Sebastián Veron",
-            address="13 y 44",
-            phone="221555232",
-            email="brujita75@hotmail.com",
+    def test_should_show_products_data(self):
+        Product.objects.create(
+            name="Producto A",
+            type="Tipo A",
+            price=100.0,
         )
 
-        Client.objects.create(
-            name="Guido Carrillo",
-            address="1 y 57",
-            phone="221232555",
-            email="goleador@gmail.com",
+        Product.objects.create(
+            name="Producto B",
+            type="Tipo B",
+            price=200.0,
         )
 
-        self.page.goto(f"{self.live_server_url}{reverse('clients_repo')}")
+        self.page.goto(f"{self.live_server_url}{reverse('products_repo')}")
 
-        expect(self.page.get_by_text("No existen clientes")).not_to_be_visible()
+        expect(self.page.get_by_text("No existen productos")).not_to_be_visible()
 
-        expect(self.page.get_by_text("Juan Sebastián Veron")).to_be_visible()
-        expect(self.page.get_by_text("13 y 44")).to_be_visible()
-        expect(self.page.get_by_text("221555232")).to_be_visible()
-        expect(self.page.get_by_text("brujita75@hotmail.com")).to_be_visible()
+        expect(self.page.get_by_text("Producto A")).to_be_visible()
+        expect(self.page.get_by_text("Tipo A")).to_be_visible()
+        expect(self.page.get_by_text("100.0")).to_be_visible()
 
-        expect(self.page.get_by_text("Guido Carrillo")).to_be_visible()
-        expect(self.page.get_by_text("1 y 57")).to_be_visible()
-        expect(self.page.get_by_text("221232555")).to_be_visible()
-        expect(self.page.get_by_text("goleador@gmail.com")).to_be_visible()
+        expect(self.page.get_by_text("Producto B")).to_be_visible()
+        expect(self.page.get_by_text("Tipo B")).to_be_visible()
+        expect(self.page.get_by_text("200.0")).to_be_visible()
 
-    def test_should_show_add_client_action(self):
-        self.page.goto(f"{self.live_server_url}{reverse('clients_repo')}")
+    def test_should_show_add_product_action(self):
+        self.page.goto(f"{self.live_server_url}{reverse('products_repo')}")
 
-        add_client_action = self.page.get_by_role(
-            "link", name="Nuevo cliente", exact=False
+        add_product_action = self.page.get_by_role(
+            "link", name="Nuevo producto", exact=False
         )
-        expect(add_client_action).to_have_attribute("href", reverse("clients_form"))
+        expect(add_product_action).to_have_attribute("href", reverse("products_form"))
 
-    def test_should_show_client_edit_action(self):
-        client = Client.objects.create(
-            name="Juan Sebastián Veron",
-            address="13 y 44",
-            phone="221555232",
-            email="brujita75@hotmail.com",
+    def test_should_show_product_edit_action(self):
+        product = Product.objects.create(
+            name="Producto A",
+            type="Tipo A",
+            price=100.0,
         )
 
-        self.page.goto(f"{self.live_server_url}{reverse('clients_repo')}")
+        self.page.goto(f"{self.live_server_url}{reverse('products_repo')}")
 
         edit_action = self.page.get_by_role("link", name="Editar")
         expect(edit_action).to_have_attribute(
-            "href", reverse("clients_edit", kwargs={"id": client.id})
+            "href", reverse("products_edit", kwargs={"id": product.id})
         )
 
-    def test_should_show_client_delete_action(self):
-        client = Client.objects.create(
-            name="Juan Sebastián Veron",
-            address="13 y 44",
-            phone="221555232",
-            email="brujita75@hotmail.com",
+    def test_should_show_product_delete_action(self):
+        product = Product.objects.create(
+            name="Producto A",
+            type="Tipo A",
+            price=100.0,
         )
 
-        self.page.goto(f"{self.live_server_url}{reverse('clients_repo')}")
+        self.page.goto(f"{self.live_server_url}{reverse('products_repo')}")
 
         edit_form = self.page.get_by_role(
-            "form", name="Formulario de eliminación de cliente"
+            "form", name="Formulario de eliminación de producto"
         )
-        client_id_input = edit_form.locator("input[name=client_id]")
+        product_id_input = edit_form.locator("input[name=product_id]")
 
         expect(edit_form).to_be_visible()
-        expect(edit_form).to_have_attribute("action", reverse("clients_delete"))
-        expect(client_id_input).not_to_be_visible()
-        expect(client_id_input).to_have_value(str(client.id))
+        expect(edit_form).to_have_attribute("action", reverse("products_delete"))
+        expect(product_id_input).not_to_be_visible()
+        expect(product_id_input).to_have_value(str(product.id))
         expect(edit_form.get_by_role("button", name="Eliminar")).to_be_visible()
 
-    def test_should_can_be_able_to_delete_a_client(self):
-        Client.objects.create(
-            name="Juan Sebastián Veron",
-            address="13 y 44",
-            phone="221555232",
-            email="brujita75@hotmail.com",
+    def test_should_can_be_able_to_delete_a_product(self):
+        Product.objects.create(
+            name="Producto A",
+            type="Tipo A",
+            price=100.0,
         )
 
-        self.page.goto(f"{self.live_server_url}{reverse('clients_repo')}")
+        self.page.goto(f"{self.live_server_url}{reverse('products_repo')}")
 
-        expect(self.page.get_by_text("Juan Sebastián Veron")).to_be_visible()
+        expect(self.page.get_by_text("Producto A")).to_be_visible()
 
         def is_delete_response(response):
-            return response.url.find(reverse("clients_delete"))
+            return response.url.find(reverse("products_delete"))
 
         # verificamos que el envio del formulario fue exitoso
         with self.page.expect_response(is_delete_response) as response_info:
@@ -162,83 +142,79 @@ class ClientsRepoTestCase(PlaywrightTestCase):
         response = response_info.value
         self.assertTrue(response.status < 400)
 
-        expect(self.page.get_by_text("Juan Sebastián Veron")).not_to_be_visible()
+        expect(self.page.get_by_text("Producto A")).not_to_be_visible()
 
 
-class ClientCreateEditTestCase(PlaywrightTestCase):
-    def test_should_be_able_to_create_a_new_client(self):
-        self.page.goto(f"{self.live_server_url}{reverse('clients_form')}")
+class ProductCreateEditTestCase(PlaywrightTestCase):
+    def test_should_be_able_to_create_a_new_product(self):
+        self.page.goto(f"{self.live_server_url}{reverse('products_form')}")
 
         expect(self.page.get_by_role("form")).to_be_visible()
 
-        self.page.get_by_label("Nombre").fill("Juan Sebastián Veron")
-        self.page.get_by_label("Teléfono").fill("221555232")
-        self.page.get_by_label("Email").fill("brujita75@hotmail.com")
-        self.page.get_by_label("Dirección").fill("13 y 44")
+        self.page.get_by_label("Nombre").fill("Producto A")
+        self.page.get_by_label("Tipo").fill("Tipo A")
+        self.page.get_by_label("Precio").fill("100.0")
 
         self.page.get_by_role("button", name="Guardar").click()
 
-        expect(self.page.get_by_text("Juan Sebastián Veron")).to_be_visible()
-        expect(self.page.get_by_text("221555232")).to_be_visible()
-        expect(self.page.get_by_text("brujita75@hotmail.com")).to_be_visible()
-        expect(self.page.get_by_text("13 y 44")).to_be_visible()
+        expect(self.page.get_by_text("Producto A")).to_be_visible()
+        expect(self.page.get_by_text("Tipo A")).to_be_visible()
+        expect(self.page.get_by_text("100.0")).to_be_visible()
 
     def test_should_view_errors_if_form_is_invalid(self):
-        self.page.goto(f"{self.live_server_url}{reverse('clients_form')}")
+        self.page.goto(f"{self.live_server_url}{reverse('products_form')}")
 
         expect(self.page.get_by_role("form")).to_be_visible()
 
         self.page.get_by_role("button", name="Guardar").click()
 
         expect(self.page.get_by_text("Por favor ingrese un nombre")).to_be_visible()
-        expect(self.page.get_by_text("Por favor ingrese un teléfono")).to_be_visible()
-        expect(self.page.get_by_text("Por favor ingrese un email")).to_be_visible()
+        expect(self.page.get_by_text("Por favor ingrese un tipo")).to_be_visible()
+        expect(self.page.get_by_text("Por favor ingrese un precio")).to_be_visible()
 
-        self.page.get_by_label("Nombre").fill("Juan Sebastián Veron")
-        self.page.get_by_label("Teléfono").fill("221555232")
-        self.page.get_by_label("Email").fill("brujita75")
-        self.page.get_by_label("Dirección").fill("13 y 44")
+        self.page.get_by_label("Nombre").fill("Producto A")
+        self.page.get_by_label("Tipo").fill("Tipo A")
+        self.page.get_by_label("Precio").fill("-100.0")
 
         self.page.get_by_role("button", name="Guardar").click()
 
         expect(self.page.get_by_text("Por favor ingrese un nombre")).not_to_be_visible()
-        expect(
-            self.page.get_by_text("Por favor ingrese un teléfono")
-        ).not_to_be_visible()
+        expect(self.page.get_by_text("Por favor ingrese un tipo")).not_to_be_visible()
+        expect(self.page.get_by_text("Por favor ingrese un precio mayor a cero")).to_be_visible()
 
-        expect(
-            self.page.get_by_text("Por favor ingrese un email valido")
-        ).to_be_visible()
-
-    def test_should_be_able_to_edit_a_client(self):
-        client = Client.objects.create(
-            name="Juan Sebastián Veron",
-            address="13 y 44",
-            phone="221555232",
-            email="brujita75@hotmail.com",
-        )
-
-        path = reverse("clients_edit", kwargs={"id": client.id})
-        self.page.goto(f"{self.live_server_url}{path}")
-
-        self.page.get_by_label("Nombre").fill("Guido Carrillo")
-        self.page.get_by_label("Teléfono").fill("221232555")
-        self.page.get_by_label("Email").fill("goleador@gmail.com")
-        self.page.get_by_label("Dirección").fill("1 y 57")
+        self.page.get_by_label("Precio").fill("0.0")
 
         self.page.get_by_role("button", name="Guardar").click()
 
-        expect(self.page.get_by_text("Juan Sebastián Veron")).not_to_be_visible()
-        expect(self.page.get_by_text("13 y 44")).not_to_be_visible()
-        expect(self.page.get_by_text("221555232")).not_to_be_visible()
-        expect(self.page.get_by_text("brujita75@hotmail.com")).not_to_be_visible()
+        expect(self.page.get_by_text("Por favor ingrese un nombre")).not_to_be_visible()
+        expect(self.page.get_by_text("Por favor ingrese un tipo")).not_to_be_visible()
+        expect(self.page.get_by_text("Por favor ingrese un precio mayor a cero")).to_be_visible()
 
-        expect(self.page.get_by_text("Guido Carrillo")).to_be_visible()
-        expect(self.page.get_by_text("1 y 57")).to_be_visible()
-        expect(self.page.get_by_text("221232555")).to_be_visible()
-        expect(self.page.get_by_text("goleador@gmail.com")).to_be_visible()
+    def test_should_be_able_to_edit_a_product(self):
+        product = Product.objects.create(
+            name="Producto A",
+            type="Tipo A",
+            price=100.0,
+        )
+
+        path = reverse("products_edit", kwargs={"id": product.id})
+        self.page.goto(f"{self.live_server_url}{path}")
+
+        self.page.get_by_label("Nombre").fill("Producto B")
+        self.page.get_by_label("Tipo").fill("Tipo B")
+        self.page.get_by_label("Precio").fill("200.0")
+
+        self.page.get_by_role("button", name="Guardar").click()
+
+        expect(self.page.get_by_text("Producto A")).not_to_be_visible()
+        expect(self.page.get_by_text("Tipo A")).not_to_be_visible()
+        expect(self.page.get_by_text("100.0")).not_to_be_visible()
+
+        expect(self.page.get_by_text("Producto B")).to_be_visible()
+        expect(self.page.get_by_text("Tipo B")).to_be_visible()
+        expect(self.page.get_by_text("200.0")).to_be_visible()
 
         edit_action = self.page.get_by_role("link", name="Editar")
         expect(edit_action).to_have_attribute(
-            "href", reverse("clients_edit", kwargs={"id": client.id})
+            "href", reverse("products_edit", kwargs={"id": product.id})
         )
