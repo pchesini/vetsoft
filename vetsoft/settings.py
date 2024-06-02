f3db804d835aa00a3b6134001ca72e793bb5546d
Francisco Lucich <franciscoivlucich@gmail.com>
2024-06-02 17:00:34 -0300

    cambios en las variables de entorno para deployar en render, agrego allowed hosts y crsf trusted origins

--- vetsoft/settings.py
+++ vetsoft/settings.py
@@ -30,7 +30,9 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.getenv("ALLOWED_HOSTS")]
+
+CSRF_TRUSTED_ORIGINS = [os.getenv("CSRF_TRUSTED_ORIGINS")]
 
 
 # Application definition
