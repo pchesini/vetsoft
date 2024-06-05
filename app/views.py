f4a2c22d12c7a0452aa6945c1fff3ab97a3d8749
Franco <francodasilva_@outlook.com>
2024-06-05 13:01:17 -0300

    Agrego docstrings a cada metodo publico

--- app/views.py
+++ app/views.py
@@ -1,18 +1,46 @@
 from django.http import HttpResponseBadRequest
 from django.shortcuts import render, redirect, reverse, get_object_or_404
-from .models import Client, Vet, Provider,Product, Medi
+from .models import Client, Vet, Provider, Product, Medi
 
 
 def home(request):
+    """
+    Renderiza la página principal.
+
+    Args:
+        request (HttpRequest): La solicitud HTTP.
+
+    Returns:
+        HttpResponse: La respuesta HTTP con la plantilla renderizada.
+    """
     return render(request, "home.html")
 
 
 def clients_repository(request):
+    """
+    Renderiza la página del repositorio de clientes.
+
+    Args:
+        request (HttpRequest): La solicitud HTTP.
+
+    Returns:
+        HttpResponse: La respuesta HTTP con la plantilla renderizada y los clientes.
+    """
     clients = Client.objects.all()
     return render(request, "clients/repository.html", {"clients": clients})
 
 
 def clients_form(request, id=None):
+    """
+    Maneja el formulario de clientes para agregar o actualizar un cliente.
+
+    Args:
+        request (HttpRequest): La solicitud HTTP.
+        id (int, optional): El ID del cliente a actualizar. Si es None, se creará un nuevo cliente.
+
+    Returns:
+        HttpResponse: La respuesta HTTP con la plantilla renderizada.
+    """
     if request.method == "POST":
         client_id = request.POST.get("id", "")
         errors = {}
@@ -39,17 +67,47 @@ def clients_form(request, id=None):
 
 
 def clients_delete(request):
+    """
+    Maneja la eliminación de un cliente.
+
+    Args:
+        request (HttpRequest): La solicitud HTTP.
+
+    Returns:
+        HttpResponse: La respuesta HTTP que redirige al repositorio de clientes.
+    """
     client_id = request.POST.get("client_id")
     client = get_object_or_404(Client, pk=int(client_id))
     client.delete()
 
     return redirect(reverse("clients_repo"))
 
+
 def products_repository(request):
+    """
+    Renderiza la página del repositorio de productos.
+
+    Args:
+        request (HttpRequest): La solicitud HTTP.
+
+    Returns:
+        HttpResponse: La respuesta HTTP con la plantilla renderizada y los productos.
+    """
     products = Product.objects.all()
     return render(request, "products/repository.html", {"products": products})
 
+
 def products_form(request, id=None):
+    """
+    Maneja el formulario de productos para agregar o actualizar un producto.
+
+    Args:
+        request (HttpRequest): La solicitud HTTP.
+        id (int, optional): El ID del producto a actualizar. Si es None, se creará un nuevo producto.
+
+    Returns:
+        HttpResponse: La respuesta HTTP con la plantilla renderizada.
+    """
     if request.method == "POST":
         product_id = request.POST.get("id", "")
         errors = {}
@@ -74,20 +132,49 @@ def products_form(request, id=None):
 
     return render(request, "products/form.html", {"product": product})
 
+
 def products_delete(request):
+    """
+    Maneja la eliminación de un producto.
+
+    Args:
+        request (HttpRequest): La solicitud HTTP.
+
+    Returns:
+        HttpResponse: La respuesta HTTP que redirige al repositorio de productos.
+    """
     product_id = request.POST.get("product_id")
     product = get_object_or_404(Product, pk=int(product_id))
     product.delete()
 
     return redirect(reverse("products_repo"))
 
+
 def vets_repository(request):
+    """
+    Renderiza la página del repositorio de veterinarios.
+
+    Args:
+        request (HttpRequest): La solicitud HTTP.
+
+    Returns:
+        HttpResponse: La respuesta HTTP con la plantilla renderizada y los veterinarios.
+    """
     vets = Vet.objects.all()
     return render(request, "vets/repository.html", {"vets": vets})
 
 
 def vets_form(request, id=None):
+    """
+    Maneja el formulario de veterinarios para agregar o actualizar un veterinario.
 
+    Args:
+        request (HttpRequest): La solicitud HTTP.
+        id (int, optional): El ID del veterinario a actualizar. Si es None, se creará un nuevo veterinario.
+
+    Returns:
+        HttpResponse: La respuesta HTTP con la plantilla renderizada.
+    """
     specialties = Vet.VetSpecialties.choices
     if request.method == "POST":
         vet_id = request.POST.get("id", "")
@@ -104,16 +191,26 @@ def vets_form(request, id=None):
             return redirect(reverse("vets_repo"))
 
         return render(
-            request, "vets/form.html", {"errors": errors, "vet": request.POST, "specialties" : specialties}
+            request, "vets/form.html", {"errors": errors, "vet": request.POST, "specialties": specialties}
         )
 
     vet = None
     if id is not None:
         vet = get_object_or_404(Vet, pk=id)
 
-    return render(request, "vets/form.html", {"vet": vet, "specialties" : specialties})
+    return render(request, "vets/form.html", {"vet": vet, "specialties": specialties})
+
 
 def vets_delete(request):
+    """
+    Maneja la eliminación de un veterinario.
+
+    Args:
+        request (HttpRequest): La solicitud HTTP.
+
+    Returns:
+        HttpResponse: La respuesta HTTP que redirige al repositorio de veterinarios.
+    """
     vet_id = request.POST.get("vet_id")
     vet = get_object_or_404(Vet, pk=int(vet_id))
     vet.delete()
@@ -121,13 +218,32 @@ def vets_delete(request):
     return redirect(reverse("vets_repo"))
 
 
-#medicine
+# Medicinas
 def medis_repository(request):
+    """
+    Renderiza la página del repositorio de medicinas.
+
+    Args:
+        request (HttpRequest): La solicitud HTTP.
+
+    Returns:
+        HttpResponse: La respuesta HTTP con la plantilla renderizada y las medicinas.
+    """
     medis = Medi.objects.all()
     return render(request, "medicine/repository.html", {"medis": medis})
 
 
 def medis_form(request, id=None):
+    """
+    Maneja el formulario de medicinas para agregar o actualizar una medicina.
+
+    Args:
+        request (HttpRequest): La solicitud HTTP.
+        id (int, optional): El ID de la medicina a actualizar. Si es None, se creará una nueva medicina.
+
+    Returns:
+        HttpResponse: La respuesta HTTP con la plantilla renderizada.
+    """
     if request.method == "POST":
         medi_id = request.POST.get("id", "")
         errors = {}
@@ -152,17 +268,49 @@ def medis_form(request, id=None):
 
     return render(request, "medicine/form.html", {"medi": medi})
 
+
 def medis_delete(request):
+    """
+    Maneja la eliminación de una medicina.
+
+    Args:
+        request (HttpRequest): La solicitud HTTP.
+
+    Returns:
+        HttpResponse: La respuesta HTTP que redirige al repositorio de medicinas.
+    """
     medi_id = request.POST.get("medi_id")
     medi = get_object_or_404(Medi, pk=int(medi_id))
     medi.delete()
 
     return redirect(reverse("medi_repo"))
+
+
 def provider_repository(request):
+    """
+    Renderiza la página del repositorio de proveedores.
+
+    Args:
+        request (HttpRequest): La solicitud HTTP.
+
+    Returns:
+        HttpResponse: La respuesta HTTP con la plantilla renderizada y los proveedores.
+    """
     provider = Provider.objects.all()
     return render(request, "provider/repository.html", {"provider": provider})
 
+
 def provider_form(request, id=None):
+    """
+    Maneja el formulario de proveedores para agregar o actualizar un proveedor.
+
+    Args:
+        request (HttpRequest): La solicitud HTTP.
+        id (int, optional): El ID del proveedor a actualizar. Si es None, se creará un nuevo proveedor.
+
+    Returns:
+        HttpResponse: La respuesta HTTP con la plantilla renderizada.
+    """
     if request.method == "POST":
         provider_id = request.POST.get("id", "")
         errors = {}
@@ -187,7 +335,17 @@ def provider_form(request, id=None):
 
     return render(request, "provider/form.html", {"provider": provider})
 
+
 def provider_delete(request):
+    """
+    Maneja la eliminación de un proveedor.
+
+    Args:
+        request (HttpRequest): La solicitud HTTP.
+
+    Returns:
+        HttpResponse: La respuesta HTTP que redirige al repositorio de proveedores.
+    """
     provider_id = request.POST.get("prov_id")
     provider = get_object_or_404(Provider, pk=int(provider_id))
     provider.delete()
