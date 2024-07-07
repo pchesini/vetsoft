bf3d73de5583f06760e31030a05f5fd06c762ac0
Pablo Chesini <pchesini1@gmail.com>
2024-07-07 17:45:11 -0300

    Saco funcion que no se usaba

--- app/models.py
+++ app/models.py
@@ -6,9 +6,9 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 
-def validate_name(name):
-    if not re.match(r'^[a-zA-Z\s]+$', name):
-        raise ValidationError('El nombre solo puede contener letras y espacios.')
+# def validate_name(name):
+#     if not re.match(r'^[a-zA-Z\s]+$', name):
+#         raise ValidationError('El nombre solo puede contener letras y espacios.')
 
 
 def validate_client(data):
